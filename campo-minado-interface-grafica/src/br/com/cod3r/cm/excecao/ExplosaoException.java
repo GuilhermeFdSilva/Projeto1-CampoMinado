bc2af5ab2491e4c7272c20c15562e2fae181cb12
GuilhermeFdSilva <francaguilherme27@gmail.com>
2022-08-02 16:57:35 -0300

    Implementação da interface gráfica

---
+++ campo-minado-interface-grafica/src/br/com/cod3r/cm/excecao/ExplosaoException.java
@@ -0,0 +1,5 @@
+package br.com.cod3r.cm.excecao;
+
+public class ExplosaoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
---
+++ campo-minado-interface-grafica/src/br/com/cod3r/cm/excecao/SairException.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.cm.excecao;
+
+public class SairException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+}
